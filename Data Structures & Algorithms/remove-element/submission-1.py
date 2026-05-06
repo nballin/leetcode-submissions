04676e1877b7204f2da68b62ecfd8be9b0ecb1ec
Nithin Balamurugan <231705275+nballin@users.noreply.github.com>
2026-05-06 19:51:27 -0400

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.py
@@ -0,0 +1,12 @@
+#output int k
+#1. k=0 iterate i
+#2. check if [i]!= val, rewrite num[k]
+#3. if it does == val, k+=1
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
+        return k
